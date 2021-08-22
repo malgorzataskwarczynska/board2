17ccaf438c2493b582282c33870e05f746666982
malgorzataskwarczynska <malgorzataskwarczynska1@gmail.com>
2021-08-22 16:53:45 +0200

    first trial

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let name = "Malgorzata";
+let secondname = "Maria";
+let surname = "Skwarczynska";
+
+console.log(name);
+console.log(secondname);
+console.log(surname);
+
+let array = ["name"; "secondname"; "surname"];
+
+
